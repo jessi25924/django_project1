36ab15d45547577815822da52c196293ce507420
jessi25924 <jessievirtucio>
2025-04-30 15:04:30 +0100

    Set Debug from False to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@yrq%1evm7%h*vc&j#)%2xlh7oy$=fa-w8wz20p-^+ma6$=dwt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
